770a7ef7c679e6cd8bde0869f79aeb17a29fb7c5
bhargav0255 <bhargav.pavan.025@gmail.com>
2024-09-08 17:53:02 +0530

    Added maximum features

--- lib/widgets/textfeild.dart
+++ lib/widgets/textfeild.dart
@@ -1,45 +1,47 @@
 import 'package:flutter/material.dart';
+
 class InputTextFeild extends StatelessWidget {
-  TextEditingController controller=TextEditingController();
+  TextEditingController controller = TextEditingController();
   final IconData icon;
   final String label;
   final bool IsObscure;
-  InputTextFeild(
-      {
-        super.key,
-        required this.icon,
-        required this.label,
-        required this.controller,
-        this.IsObscure=false,
-      }
-      );
+  InputTextFeild({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.controller,
+    this.IsObscure = false,
+  });
   @override
   Widget build(BuildContext context) {
     return TextField(
-      style: const TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
+      style: const TextStyle(fontSize: 16),
       obscuringCharacter: '*',
       obscureText: IsObscure,
       controller: controller,
       decoration: InputDecoration(
         focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(
-            width:2,
-            color: Colors.orange,
+          borderSide: BorderSide(
+            width: 1,
+            color: Colors.orange.shade200,
             style: BorderStyle.solid,
           ),
-          borderRadius: BorderRadius.circular(15),
+          borderRadius: BorderRadius.circular(10),
         ),
-        enabledBorder:OutlineInputBorder(
-          borderSide: const BorderSide(
-            width:2,
-            color: Colors.orange,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: Colors.orange.shade100,
             style: BorderStyle.solid,
           ),
-          borderRadius: BorderRadius.circular(15),
+          borderRadius: BorderRadius.circular(10),
         ),
-        hintStyle: const TextStyle(fontWeight: FontWeight.bold,fontSize: 18),
+        hintStyle: const TextStyle(fontSize: 16),
         hintText: label,
-        prefixIcon: Icon(icon,color: Colors.orangeAccent,),
+        prefixIcon: Icon(
+          icon,
+          color: Colors.orangeAccent.shade200,
+        ),
       ),
     );
   }
