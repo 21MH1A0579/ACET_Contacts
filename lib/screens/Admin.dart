03ca511839d0beb5d40dc443b55d8f9325ab7006
bhargav0255 <bhargav.pavan.025@gmail.com>
2024-09-08 22:32:16 +0530

    Added add to contact functionality with changes in android contact access functions

--- lib/screens/Admin.dart
+++ lib/screens/Admin.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:aditya_contacts/screens/AdminMain.dart';
 import 'package:aditya_contacts/screens/userprofile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
